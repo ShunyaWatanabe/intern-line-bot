44a90ec79ef9460775a61c1941eeb7f8e47a4819
gifteeintern-D <s.watte3212@gmail.com>
2019-06-19 13:42:54 +0900

    added get random chinese function

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -7,6 +7,7 @@ class WebhookController < ApplicationController
   SOURCE = 'ja'
   TARGET = 'zh-cn'
   ROOT = ENV["TRANSLATION_ROOT_URI"]
+  CHINESE_URI = 'http://chugokugo-script.net/tango/level3/meishi.html'
   protect_from_forgery except: [:callback] # CSRF対策無効化
 
   def client
@@ -32,7 +33,8 @@ class WebhookController < ApplicationController
         when Line::Bot::Event::MessageType::Text
           case event.message['text']
           when '新しい単語'
-            client.reply_message(event['replyToken'], format_message('test'))
+            message = get_random_chinese()
+            client.reply_message(event['replyToken'], format_message(message))
           else
             uri = URI.encode("#{ROOT}?text=#{event.message['text']}&source=#{SOURCE}&target=#{TARGET}")
             res = get_json_translation(URI.parse(uri))
@@ -50,7 +52,30 @@ class WebhookController < ApplicationController
 
   private
   def get_random_chinese()
-    puts 'get_random_chinese'
+    charset = nil
+    html = open(CHINESE_URI) do |f|
+      charset = f.charset
+      f.read
+    end    
+    
+    # 文字化け対応
+    if charset == "iso-8859-1"
+      charset = html.scan(/charset="?([^\s"]*)/i).first.join
+    end
+
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+    lines = doc.xpath('//section/div[@class="divBunruiBox"]')
+    rand_num = rand(lines.count)
+    rand_line = lines[rand_num]
+
+    chinese_word = rand_line.css('.divBunruiC').inner_text
+    pinyin_word = rand_line.css('.divBunruiP').inner_text
+    japanese_word = rand_line.css('.divBunruiN').inner_text
+    chinese_sentence = rand_line.css('.divBunruiExC').inner_text
+    pinyin_sentence = rand_line.css('.divBunruiExP').inner_text
+    japanese_sentence = rand_line.css('.divBunruiExN').inner_text
+    
+    "新しい単語：#{chinese_word}\n発音：#{pinyin_word}\n意味：#{japanese_word}\n例文：\n\t#{chinese_sentence}\n\t#{pinyin_sentence}\n\t#{japanese_sentence}"
   end
 
   def format_message(message)
