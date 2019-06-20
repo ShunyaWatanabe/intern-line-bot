78cd2f8f9a18973085da4ed3d41b5d8b1d4dd7e7
gifteeintern-D <s.watte3212@gmail.com>
2019-06-20 13:41:09 +0900

    改行直したのとseedsを修正

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,68 +1,67 @@
 class Word < ApplicationRecord
-    SOURCE = 'ja'
-    TARGET = 'zh-cn'
-    TRANSLATION_ROOT_URI = ENV["TRANSLATION_ROOT_URI"]
-    CHINESE_WORDS_URI = 'http://chugokugo-script.net/tango/level3/meishi.html'
-    has_one :sentence
-    validates :chinese, presence: true
-    validates :japanese, presence: true
-    validates :pinyin, presence: true
-    validates :level, presence: true
+  SOURCE = 'ja'
+  TARGET = 'zh-cn'
+  TRANSLATION_ROOT_URI = ENV["TRANSLATION_ROOT_URI"]
+  CHINESE_WORDS_URI = 'http://chugokugo-script.net/tango/level3/meishi.html'
+  has_one :sentence
+  validates :chinese, presence: true
+  validates :japanese, presence: true
+  validates :pinyin, presence: true
+  validates :level, presence: true
 
-    def self.get_response_message(text)
-        case text
-        when '新しい単語'
-          random_chinese = get_random_chinese
-          format_message(random_chinese)
-        else
-          uri = URI.encode("#{TRANSLATION_ROOT_URI}?text=#{text}&source=#{SOURCE}&target=#{TARGET}")
-          translation = get_json_translation(URI.parse(uri))
-          format_message(translation['message'])
-        end 
+  def self.get_response_message
+    if text.include?('新しい単語')
+      random_chinese = get_random_chinese
+      format_message(random_chinese)
+    else
+      uri = URI.encode("#{TRANSLATION_ROOT_URI}?text=#{text}&source=#{SOURCE}&target=#{TARGET}")
+      translation = get_json_translation(URI.parse(uri))
+      format_message(translation['message'])
+    end 
+  end
+
+  private
+  def self.get_random_chinese
+    res = random_chinese_from_db
+    message = <<~EOS
+      新しい単語：#{res[:word].chinese}
+      発音：#{res[:word].pinyin}
+      意味：#{res[:word].japanese}
+      例文：
+    #{res[:sentence].chinese}
+    #{res[:sentence].pinyin}
+    #{res[:sentence].japanese}
+    EOS
+    message.chomp
+  end
+
+  def self.random_chinese_from_db
+    chinese_word = Word.find(Word.pluck(:id).sample)
+    chinese_sentence = chinese_word.sentence
+    {
+      word: chinese_word,
+      sentence: chinese_sentence
+    }
+  end
+
+  def self.format_message(message)
+    {
+      type: 'text',
+      text: message
+    }
+  end
+
+  def self.get_translation(uri)
+    response = Net::HTTP.get_response(uri)
+    case response
+    when Net::HTTPRedirection
+      get_translation(URI.parse(response["location"]))
+    else
+      response.body
     end
-
-    private
-    def self.get_random_chinese
-        res = random_chinese_from_db
-        message = <<~EOS
-              新しい単語：#{res[:word].chinese}
-              発音：#{res[:word].pinyin}
-              意味：#{res[:word].japanese}
-              例文：
-                #{res[:sentence].chinese}
-                #{res[:sentence].pinyin}
-                #{res[:sentence].japanese}
-            EOS
-        message.chomp
-    end
-
-    def self.random_chinese_from_db
-        chinese_word = Word.find(Word.pluck(:id).sample)
-        chinese_sentence = chinese_word.sentence
-        {
-            word: chinese_word,
-            sentence: chinese_sentence
-        }
-    end
-
-    def self.format_message(message)
-        {
-          type: 'text',
-          text: message
-        }
-    end
-
-    def self.get_translation(uri)
-        response = Net::HTTP.get_response(uri)
-        case response
-        when Net::HTTPRedirection
-          get_translation(URI.parse(response["location"]))
-        else
-          response.body
-        end
-      end
+  end
     
-    def self.get_json_translation(uri)
-        JSON.parse(get_translation(uri)) # -> レスポンスをjsonで変換して返してくれる
-    end
+  def self.get_json_translation(uri)
+    JSON.parse(get_translation(uri)) # -> レスポンスをjsonで変換して返してくれる
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# 間違っていたので変更した。変更後確認はしていない
 5.times do |no|
   User.create()
-  Sentence.create(japanese: no.to_s, chinese: no.to_s, pinyin: no.to_s)
   Word.create(japanese: no.to_s, chinese: no.to_s, pinyin: no.to_s, level:3)
+  Sentence.create(word_id: no+1, japanese: no.to_s, chinese: no.to_s, pinyin: no.to_s)
   RememberedWord.create(user_id: no+1, word_id: no+1)
 end
