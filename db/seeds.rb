8ce45a8712601864e5c06c8134a4b0a5dfa6e438
gifteeintern-D <s.watte3212@gmail.com>
2019-06-20 11:38:25 +0900

    added seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do |no|
+  User.create()
+  Sentence.create(japanese: no.to_s, chinese: no.to_s, pinyin: no.to_s)
+  Word.create(japanese: no.to_s, chinese: no.to_s, pinyin: no.to_s, level:no+1)
+  RememberedWord.create(user_id: no+1, word_id: no+1)
+end
