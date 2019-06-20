41601989085a35d89b4804c6d0445312b19a5884
gifteeintern-D <s.watte3212@gmail.com>
2019-06-20 14:25:08 +0900

    seeds修正とレベル分けてメッセの送信

--- app/models/word.rb
+++ app/models/word.rb
@@ -9,20 +9,27 @@ class Word < ApplicationRecord
   validates :pinyin, presence: true
   validates :level, presence: true
 
-  def self.get_response_message
+  def self.get_response_message(text)
     if text.include?('新しい単語')
-      random_chinese = get_random_chinese
+      random_chinese = get_random_chinese(text)
       format_message(random_chinese)
     else
       uri = URI.encode("#{TRANSLATION_ROOT_URI}?text=#{text}&source=#{SOURCE}&target=#{TARGET}")
       translation = get_json_translation(URI.parse(uri))
       format_message(translation['message'])
-    end 
+    end
   end
 
   private
-  def self.get_random_chinese
-    res = random_chinese_from_db
+  def self.get_random_chinese(text)
+    level = 3
+    if text.include?('2')
+      level = 2
+    elsif text.include?('1')
+      level = 1
+    end
+
+    res = random_chinese_from_db(level)
     message = <<~EOS
       新しい単語：#{res[:word].chinese}
       発音：#{res[:word].pinyin}
@@ -35,8 +42,8 @@ class Word < ApplicationRecord
     message.chomp
   end
 
-  def self.random_chinese_from_db
-    chinese_word = Word.find(Word.pluck(:id).sample)
+  def self.random_chinese_from_db(level)
+    chinese_word = Word.find(Word.where("level=?",level).pluck(:id).sample)
     chinese_sentence = chinese_word.sentence
     {
       word: chinese_word,
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# 間違っていたので変更した。変更後確認はしていない
 5.times do |no|
   User.create()
   Word.create(japanese: no.to_s, chinese: no.to_s, pinyin: no.to_s, level:3)
   Sentence.create(word_id: no+1, japanese: no.to_s, chinese: no.to_s, pinyin: no.to_s)
   RememberedWord.create(user_id: no+1, word_id: no+1)
 end
+
+Word.create(japanese: 'レベル１日本語', chinese: 'レベル１中国語', pinyin: 'レベル１ピンイン', level:1)
+Word.create(japanese: 'レベル２日本語', chinese: 'レベル２中国語', pinyin: 'レベル２ピンイン', level:2)
+
+Sentence.create(word_id:6, japanese: 'レベル１例文', chinese: 'レベル１例文', pinyin: 'レベル１例文')
+Sentence.create(word_id:7, japanese: 'レベル２例文', chinese: 'レベル２例文', pinyin: 'レベル２例文')
